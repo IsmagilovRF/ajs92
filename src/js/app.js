eebf1729f2fdad249762cf85b326c5912a553f05
IsmagilovRF <ledohod@icloud.com>
2021-01-30 23:52:54 +0300

    edit files

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,17 @@
 export default class ArrayBufferConverter {
-    constructor(buffer) {
-      this.buffer = buffer;
-      this.string = '';
-    }
-  
-    load() {
-      this.bufferView = new Uint16Array(this.buffer);
-    }
-  
-    toString() {
-      for (let i = 0; i < this.bufferView.length; i += 1) {
-        this.string += String.fromCharCode(this.bufferView[i]);
-      }
-      return this.string;
-    }
+  constructor(buffer) {
+    this.buffer = buffer;
+    this.string = '';
   }
-  
+
+  load() {
+    this.bufferView = new Uint16Array(this.buffer);
+  }
+
+  toString() {
+    for (let i = 0; i < this.bufferView.length; i += 1) {
+      this.string += String.fromCharCode(this.bufferView[i]);
+    }
+    return this.string;
+  }
+}
